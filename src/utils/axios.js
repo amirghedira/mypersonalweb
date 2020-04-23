e3b81f1757e3ada316333a225752015bb54a87df
amir ghedira <amirghedira06@gmail.com>
2020-04-23 15:04:44 +0100

    V2.2

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instanceaxios = axios.create({
-    baseURL: 'https://mywebrestapi.herokuapp.com'
+    baseURL: 'http://localhost:5000'
 })
 
 export default instanceaxios
